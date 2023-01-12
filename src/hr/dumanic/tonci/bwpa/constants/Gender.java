3c8cb74451bdafa89f6a7a736888890d0248ff26
TonciDumanic <45033808+TonciDumanic@users.noreply.github.com>
2023-01-12 10:09:33 +0100

    Add files via upload

---
+++ src/hr/dumanic/tonci/bwpa/constants/Gender.java
@@ -0,0 +1,11 @@
+package hr.dumanic.tonci.bwpa.constants;
+
+public class Gender {
+
+	private Gender() {}
+	
+	public static final int MALE = 0;
+	public static final int FEMALE = 1;
+	public static final int OTHER = 2;
+
+}
