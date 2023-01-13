b2e7b8660f81d5fae7f3f6bec152714bb9f0fafb
etondum <tonci.dumanic@ericsson.com>
2023-01-13 09:51:22 +0100

    Change-Id: I0000000000000000000000000000000000000000
    Change-Id: Ie39b4ff55bc19a4d877de514391c81805ff302e6

---
+++ src/hr/dumanic/tonci/bwpa/constants/KidFriendlyStatus.java
@@ -0,0 +1,12 @@
+package hr.dumanic.tonci.bwpa.constants;
+
+public class KidFriendlyStatus {
+	
+	private void KidFriendlyStatus() {}
+	
+	public static final String APPROVED = "approved";
+	public static final String REJECTED = "rejected";
+	public static final String UNKNOWN = "unknown";
+
+
+}
---
+++ src/hr/dumanic/tonci/bwpa/partner/package-info.java
@@ -0,0 +1,1 @@
+package hr.dumanic.tonci.bwpa.partner;
