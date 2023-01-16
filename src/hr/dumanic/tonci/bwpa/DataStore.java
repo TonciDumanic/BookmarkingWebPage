664f6234eda99c6962ff2c07c35a6f1b90269e4f
TonciDumanic <tonci.dumanic@gmail.com>
2023-01-16 11:20:06 +0100

    Added load for books and movies from fiels
    Change-Id: Ieb93207df78787122fead0a13025d398c9ebb87b

--- src/hr/dumanic/tonci/bwpa/DataStore.java
+++ src/hr/dumanic/tonci/bwpa/DataStore.java
@@ -58,7 +58,7 @@ public class DataStore {
 		bookmarks[0][4] = BookmarkManager.getInstance().createWebLink(2004,"Virtual Hosting and Tomcat","http://tomcat.apache.org/tomcat-6.0-doc/virtual-hosting-howto.html","http://tomcat.apache.org");
 */
 		String[] data = new String[BOOKMARK_COUNT_PER_TYPE];
-		IOUtil.read(data, "User");
+		IOUtil.read(data, "WebLink");
 		int rowNumber = 0;
 		
 		for(String row: data) {
@@ -70,31 +70,47 @@ public class DataStore {
 	}
 	
 	private static void loadMovies() {
-		bookmarks[1][0] = BookmarkManager.getInstance().createMovie(3000,"Citizen Kane","",1941,new String[] {"Orson Welles","Joseph Cotten"},new String[] {"Orson Welles"},MovieGenre.CLASSICS,8.5);
+		/*bookmarks[1][0] = BookmarkManager.getInstance().createMovie(3000,"Citizen Kane","",1941,new String[] {"Orson Welles","Joseph Cotten"},new String[] {"Orson Welles"},MovieGenre.CLASSICS,8.5);
 		bookmarks[1][1] = BookmarkManager.getInstance().createMovie(3001,"The Grapes of Wrath","",1940,new String[] {"Henry Fonda","Jane Darwell"},new String[] {"John Ford"},MovieGenre.CLASSICS,8.2);
 		bookmarks[1][2] = BookmarkManager.getInstance().createMovie(3002,"A Touch of Greatness","",2004,new String[] {"Albert Cullum"},new String[] {"Leslie Sullivan"},MovieGenre.DOCUMENTARIES,7.3);
 		bookmarks[1][3] = BookmarkManager.getInstance().createMovie(3003,"The Big Bang Theory","",2007,new String[] {"Kaley Cuoco","Jim Parsons"},new String[] {"Chuck Lorre","Bill Prady"},MovieGenre.TV_SHOWS,8.7);
 		bookmarks[1][4] = BookmarkManager.getInstance().createMovie(3004,"Ikiru","",1952,new String[] {"Takashi Shimura","Minoru Chiaki"},new String[] {"Akira Kurosawa"},MovieGenre.FOREIGN_MOVIES,8.4);
-		
-		/*String[] data = new String[BOOKMARK_COUNT_PER_TYPE];
-		IOUtil.read(data, "User");
+		*/
+		String[] data = new String[BOOKMARK_COUNT_PER_TYPE];
+		IOUtil.read(data, "Movie");
 		int rowNumber = 0;
 		
 		for(String row: data) {
 			String[] values = row.split("\t");
 		
-				
-			bookmarks[1][rowNumber++] = BookmarkManager.getInstance().createMovie(Long.parseLong(values[0]), values[1], Integer.parseInt(values[2]), , values, data, row, rowNumber)
-		}*/
+			String[] cast =  values[3].split(",");
+			String[] directors =  values[4].split(",");
+			
+			
+					
+			bookmarks[1][rowNumber++] = BookmarkManager.getInstance().createMovie(Long.parseLong(values[0]), values[1],"", Integer.parseInt(values[2]), cast, directors, values[5], Double.parseDouble(values[6]));
+		}
 	}
 
 	private static void loadBooks() {
-		bookmarks[2][0] = BookmarkManager.getInstance().createBook(4000,"Walden",1854,"Wilder Publications",new String [] {"Henry David Thoreau"},BookGenre.PHILOSOPHY,4.3);
+		/*bookmarks[2][0] = BookmarkManager.getInstance().createBook(4000,"Walden",1854,"Wilder Publications",new String [] {"Henry David Thoreau"},BookGenre.PHILOSOPHY,4.3);
 		bookmarks[2][1] = BookmarkManager.getInstance().createBook(4001,"Self-Reliance and Other Essays",1993,"Dover Publications",new String [] {"Ralph Waldo Emerson"},BookGenre.PHILOSOPHY,4.5);
 		bookmarks[2][2] = BookmarkManager.getInstance().createBook(4002,"Light From Many Lamps",1988,"Touchstone",new String [] {"Lillian Eichler Watson"},BookGenre.PHILOSOPHY,5.0);
 		bookmarks[2][3] = BookmarkManager.getInstance().createBook(4003,"Head First Design Patterns",2004,"O'Reilly Media",new String [] {"Eric Freeman","Bert Bates","Kathy Sierra","Elisabeth Robson"},BookGenre.TECHNICAL,4.5);
 		bookmarks[2][4] = BookmarkManager.getInstance().createBook(4004,"Effective Java Programming Language Guide",2007,"Prentice Hall",new String [] {"Joshua Bloch"},BookGenre.TECHNICAL,4.9);
-
+*/
+		String[] data = new String[BOOKMARK_COUNT_PER_TYPE];
+		IOUtil.read(data, "Book");
+		int rowNumber = 0;
+		
+		for(String row: data) {
+			String[] values = row.split("\t");
+		
+			String[] authors =  values[4].split(",");
+					
+			bookmarks[2][rowNumber++] = BookmarkManager.getInstance().createBook(Long.parseLong(values[0]), values[1], Integer.parseInt(values[2]), values[3], authors, values[5], Double.parseDouble(values[6])/*, values[7]*/);
+		}
+		
 	}
 	public static void loadData() {
 		loadUsers();
